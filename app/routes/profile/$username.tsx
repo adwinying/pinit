1549ceabb431578d79913bb190b3b8b5d7d2cb19
Adwin Ying <adwinying@gmail.com>
2022-09-05 10:10:23 +0900

    Add profile page

---
+++ app/routes/profile/$username.tsx
@@ -0,0 +1,87 @@
+import type { LoaderFunction } from "@remix-run/node"
+import { json } from "@remix-run/node"
+import { useLoaderData } from "@remix-run/react"
+
+import PageTitle from "~/components/PageTitle"
+import Pagination from "~/components/Pagination"
+import PinsGrid from "~/components/PinsGrid"
+import { getUserByUsername } from "~/libs/getUserByUsername"
+import { getUserPins } from "~/libs/getUserPins"
+import { getUserPinsCount } from "~/libs/getUserPinsCount"
+import { getUser } from "~/utils/auth.server"
+
+export type LoaderData = {
+  user: {
+    id: string
+    username: string
+  }
+  isOwner: boolean
+  pinCount: number
+  pins: {
+    id: string
+    title: string
+    imageUrl: string
+    username: string
+    userImgUrl: string
+  }[]
+  pagination: {
+    baseUrl: string
+    perPage: number
+    currentPage: number
+    total: number
+  }
+}
+export const loader: LoaderFunction = async ({ params, request }) => {
+  const { username } = params
+
+  if (!username) throw new Response("Not Found", { status: 404 })
+
+  const user = await getUserByUsername({ username })
+
+  if (user === null) throw new Response("Not Found", { status: 404 })
+
+  const authUser = await getUser(request)
+  const pinCount = await getUserPinsCount({ user })
+
+  const url = new URL(request.url)
+  const count = 20
+  const page = Number(url.searchParams.get("page") ?? 1)
+  const offset = (page - 1) * count
+  const pins = await getUserPins({ user, offset, count })
+
+  return json<LoaderData>({
+    user: {
+      id: user.id,
+      username: user.username,
+    },
+    isOwner: user.id === authUser?.id,
+    pinCount,
+    pins: pins.map(({ id, title, imageUrl, owner }) => ({
+      id,
+      title,
+      imageUrl,
+      username: owner.username,
+      userImgUrl: owner.profileImgUrl,
+    })),
+    pagination: {
+      baseUrl: url.toString(),
+      perPage: count,
+      currentPage: page,
+      total: pinCount,
+    },
+  })
+}
+
+export default function UserProfile() {
+  const { user, pins, pagination } = useLoaderData<LoaderData>()
+
+  return (
+    <div>
+      <PageTitle>{user.username}'s Pins</PageTitle>
+
+      <PinsGrid pins={pins} className="my-5" />
+
+      <Pagination {...pagination} />
+    </div>
+  )
+}
---
+++ tests/integrations/profile.spec.ts
@@ -0,0 +1,206 @@
+import { installGlobals } from "@remix-run/node"
+
+import { prepareRealDb } from "~/../tests/realDb"
+import { loader } from "~/routes/profile/$username"
+import db from "~/utils/db.server"
+
+describe("profile page", () => {
+  beforeEach(async () => {
+    installGlobals()
+    prepareRealDb()
+  })
+
+  it("shows not found if username undefined", async () => {
+    try {
+      await loader({
+        request: new Request("http:///profile"),
+        params: {},
+        context: {},
+      })
+
+      assert.fail()
+    } catch (e) {
+      if (!(e instanceof Response)) assert.fail()
+
+      expect(e.status).toEqual(404)
+    }
+  })
+
+  it("shows not found if username does not exist", async () => {
+    try {
+      await loader({
+        request: new Request("http:///profile"),
+        params: { username: "foo" },
+        context: {},
+      })
+
+      assert.fail()
+    } catch (e) {
+      if (!(e instanceof Response)) assert.fail()
+
+      expect(e.status).toEqual(404)
+    }
+  })
+
+  const userData = {
+    username: "foo",
+    twitterId: "foo",
+    profileImgUrl: "http://test.com/test.png",
+  }
+
+  it("returns user info", async () => {
+    const user = await db.user.create({ data: userData })
+
+    const request = new Request("http:///profile")
+    const response: Response = await loader({
+      request,
+      params: { username: userData.username },
+      context: {},
+    })
+    const loaderData = await response.json()
+
+    const expected = {
+      id: user.id,
+      username: user.username,
+    }
+
+    expect(response.status).toEqual(200)
+    expect(loaderData.user).toEqual(expected)
+  })
+
+  it("returns empty pins array if user created no pins", async () => {
+    await db.user.create({ data: userData })
+
+    const request = new Request("http:///profile")
+    const response: Response = await loader({
+      request,
+      params: { username: userData.username },
+      context: {},
+    })
+    const loaderData = await response.json()
+
+    expect(response.status).toEqual(200)
+    expect(loaderData.pins).toHaveLength(0)
+  })
+
+  it("returns all pins created by user", async () => {
+    const pinsData = Array.from({ length: 20 }).map((_, i) => ({
+      title: `Pin ${i + 1}`,
+      imageUrl: "http://test.com/test.png",
+    }))
+
+    const user = await db.user.create({ data: userData })
+    await Promise.all(
+      pinsData.map((pin) =>
+        db.pin.create({ data: { ...pin, ownerId: user.id } }),
+      ),
+    )
+    const pins = await db.pin.findMany({
+      orderBy: { updatedAt: "desc" },
+    })
+
+    const request = new Request("http:///profile")
+    const response: Response = await loader({
+      request,
+      params: { username: userData.username },
+      context: {},
+    })
+    const loaderData = await response.json()
+
+    const expected = pins.map(({ id, title, imageUrl }) => ({
+      id,
+      title,
+      imageUrl,
+      username: user.username,
+      userImgUrl: user.profileImgUrl,
+    }))
+
+    expect(response.status).toEqual(200)
+    expect(loaderData.pins).toEqual(expected)
+  })
+
+  it("returns pagination params", async () => {
+    await db.user.create({ data: userData })
+
+    const request = new Request("http:///profile")
+    const response: Response = await loader({
+      request,
+      params: { username: userData.username },
+      context: {},
+    })
+    const loaderData = await response.json()
+
+    const expected = {
+      baseUrl: request.url,
+      perPage: 20,
+      currentPage: 1,
+      total: 0,
+    }
+
+    expect(response.status).toEqual(200)
+    expect(loaderData.pagination).toEqual(expected)
+  })
+
+  it("returns max 20 pins", async () => {
+    const pinsData = Array.from({ length: 21 }).map((_, i) => ({
+      title: `Pin ${i + 1}`,
+      imageUrl: "http://test.com/test.png",
+    }))
+
+    const user = await db.user.create({ data: userData })
+    await Promise.all(
+      pinsData.map((pin) =>
+        db.pin.create({ data: { ...pin, ownerId: user.id } }),
+      ),
+    )
+
+    const request = new Request("http:///profile")
+    const response: Response = await loader({
+      request,
+      params: { username: userData.username },
+      context: {},
+    })
+    const loaderData = await response.json()
+
+    expect(response.status).toEqual(200)
+    expect(loaderData.pins).toHaveLength(20)
+  })
+
+  it("returns next page's pins when query params defined", async () => {
+    const pinsData = Array.from({ length: 25 }).map((_, i) => ({
+      title: `Pin ${i + 1}`,
+      imageUrl: "http://test.com/test.png",
+    }))
+
+    const user = await db.user.create({ data: userData })
+    await Promise.all(
+      pinsData.map((pin) =>
+        db.pin.create({ data: { ...pin, ownerId: user.id } }),
+      ),
+    )
+
+    const request = new Request("http:///profile?page=2")
+    const response: Response = await loader({
+      request,
+      params: { username: userData.username },
+      context: {},
+    })
+    const loaderData = await response.json()
+
+    const page2Pins = await db.pin.findMany({
+      skip: 20,
+      orderBy: { updatedAt: "desc" },
+    })
+    const expected = page2Pins.map(({ id, title, imageUrl }) => ({
+      id,
+      title,
+      imageUrl,
+      username: user.username,
+      userImgUrl: user.profileImgUrl,
+    }))
+
+    expect(response.status).toEqual(200)
+    expect(loaderData.pins).toHaveLength(5)
+    expect(loaderData.pins).toEqual(expected)
+  })
+})
